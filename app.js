84b842b93e89ede1c87a58c232b2cd8f5091534b
Mario Dorn <mario.dorn@forgefire.net>
2017-10-04 07:45:48 +0200

    Add a simple logger middleware for koa

--- app.js
+++ app.js
@@ -1,38 +1,32 @@
 'use strict';
 
 const Koa = require('koa');
-const db = require('./models');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const route = require('koa-route');
 const repl = require('repl');
-
-db.sequelize.sync({force: true})
-  .then(() => {
-    repl.start('REPL> ').context.db = db;
-  });
-
+const db = require('./models');
 const app = new Koa();
+const timer = function () {
+  return async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+  };
+}
 
-// x-response-time
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
-
-// logger
-
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-});
-
-// response
-
-app.use(async ctx => {
+db.sequelize
+  .sync({force: true})
+  .then(() => {
+    repl.start('REPL> \n').context['db'] = db;
+  });
+app.context['db'] = db;
+app.use(timer());
+app.use(logger());
+app.use(serve(__dirname + '/public'));
+app.use(route.get('/hello', ctx => {
   ctx.body = 'Hello World';
-});
+}));
 
 module.exports = app;
