fa7c30714dd5be70d67da1d63fba70e12ddfbdfd
Mario Dorn <mario.dorn@forgefire.net>
2017-10-17 04:49:58 +0200

    Update some models and populate playground

--- models/core.js
+++ models/core.js
@@ -5,11 +5,11 @@ module.exports = (sequelize, DataTypes) => {
     label: DataTypes.STRING,
     latitude: DataTypes.DECIMAL(8, 6),
     longitude: DataTypes.DECIMAL(9, 6),
-    core_type: DataTypes.STRING, // TODO: Swap out to separate table
+    coring_method: DataTypes.STRING, // TODO: Swap out to separate table
     water_depth: DataTypes.DECIMAL,
     depth_start: DataTypes.DECIMAL,
     depth_end: DataTypes.DECIMAL,
-    core_length: DataTypes.DECIMAL, // TODO: Compute this value
+    length: DataTypes.DECIMAL,
     drill_date: DataTypes.DATEONLY,
   }, {
     underscored: true,
