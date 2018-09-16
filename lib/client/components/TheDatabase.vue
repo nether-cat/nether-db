a370ef1a26a8cf63657021b83c099f9c3ef3ed35
Mario Dorn <mario.dorn@forgefire.net>
2018-09-16 11:35:26 +0200

    WIP: Update models and design the output from database

--- lib/client/components/TheDatabase.vue
+++ lib/client/components/TheDatabase.vue
@@ -38,9 +38,43 @@
               </b-progress>
               <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" data-projection="EPSG:4326" style="height: 400px">
                 <vl-view :zoom.sync="map.zoom" :center.sync="map.center" :rotation.sync="map.rotation"></vl-view>
+                <vl-interaction-select :features.sync="map.selected">
+                  <!--suppress HtmlUnknownAttribute -->
+                  <template slot-scope="select">
+                    <!--vl-overlay :position="findPointOnSurface(feature.geometry)"-->
+                    <vl-overlay v-for="feature in select.features"
+                                class="feature-popup feature-popup-lake"
+                                :key="feature.id"
+                                :id="feature.id"
+                                :auto-pan="true"
+                                :position="[-90, -180]">
+                      <!--suppress HtmlUnknownAttribute -->
+                      <template slot-scope="popup">
+                        <b-card style="position: absolute; bottom: .5em; left: .5em; width: 200px">
+                          <div slot="header">
+                            <span style="float: left; font-size: 1rem; padding: 0 1rem .25rem 0">
+                              {{ feature.properties.name }}
+                            </span>
+                            <button type="button" class="close" aria-label="Close" style="font-size: 1.25rem"
+                                    @click="map.selected = map.selected.filter(f => f.id !== feature.id)">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          Open details
+                        </b-card>
+                      </template>
+                    </vl-overlay>
+                  </template>
+                </vl-interaction-select>
                 <vl-layer-tile id="osm">
                   <vl-source-osm></vl-source-osm>
                 </vl-layer-tile>
+                <vl-feature v-for="result in transformedResults"
+                            :result="result"
+                            :key="result.id"
+                            :properties="{ name: result.name }">
+                  <vl-geom-point :coordinates="[result.longitude, result.latitude]"></vl-geom-point>
+                </vl-feature>
               </vl-map>
             </no-ssr>
             </div>
@@ -51,7 +85,7 @@
     <b-row>
       <b-col class="mt-4">
         <b-card>
-          <b-table hover outlined striped caption-top :items="transformResults">
+          <b-table hover outlined striped caption-top :items="transformedResults" :fields="fields">
             <template slot="table-caption">
               This is a table caption.
             </template>
@@ -70,6 +104,16 @@
 
   const noSSR = {};
 
+  let callFindPointOnSurface = undefined;
+
+  let findPointOnSurface = (geom) => {
+    if (callFindPointOnSurface) {
+      return callFindPointOnSurface(geom);
+    } else {
+      console.error('Function "findPointOnSurface" was not loaded correctly!');
+    }
+  };
+
   if (process.env.VUE_ENV !== 'server') {
     const fallback = {
       template: `
@@ -80,12 +124,18 @@
       </div>`,
     };
     const vlLoad = import('vuelayers').catch(e => console.error(e));
+    const vlLoadExt = import('vuelayers/lib/ol-ext').catch(e => console.error(e));
+    vlLoadExt.then(module => callFindPointOnSurface = module.findPointOnSurface).catch(() => {});
     // noinspection JSUnusedGlobalSymbols
     Object.assign(noSSR, {
       vlMap: () => vlLoad.then(module => module.Map['Map']).catch(() => fallback),
       vlView: () => vlLoad.then(module => module.Map['View']).catch(() => fallback),
+      vlInteractionSelect: () => vlLoad.then(module => module.SelectInteraction['Interaction']).catch(() => fallback),
+      vlOverlay: () => vlLoad.then(module => module.Overlay['Overlay']).catch(() => fallback),
       vlLayerTile: () => vlLoad.then(module => module.TileLayer['Layer']).catch(() => fallback),
       vlSourceOsm: () => vlLoad.then(module => module.OsmSource['Source']).catch(() => fallback),
+      vlFeature: () => vlLoad.then(module => module.Feature['Feature']).catch(() => fallback),
+      vlGeomPoint: () => vlLoad.then(module => module.PointGeom['Geom']).catch(() => fallback),
     });
   }
 
@@ -166,7 +216,15 @@
           zoom: 1,
           center: [0, 20],
           rotation: 0,
+          selected: [],
         },
+        fields: [
+          'name',
+          'longitude',
+          'latitude',
+          'surface_level',
+          'max_depth',
+        ],
       };
     },
     computed: {
@@ -178,12 +236,9 @@
         'results',
       ]),
       ...mapGetters('database', [
-        'countProxies'
+        'countProxies',
+        'transformedResults',
       ]),
-      transformResults () {
-        //debugger;
-        return Object.entries(this['results']);
-      },
     },
     methods: {
       ...mapActions('database', [
@@ -191,10 +246,16 @@
         'loadProxyAttributes',
         'loadResultData',
       ]),
+      findPointOnSurface,
     },
   };
 </script>
 
 <style lang="scss" scoped>
   @import '~vuelayers/lib/style.css';
+
+  .feature-popup-lake {
+    bottom: .5em;
+    left: .5em;
+  }
 </style>
