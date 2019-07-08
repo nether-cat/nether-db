befa00c12e4779ae370700d0721ce5b1405b7254
Mario Dorn <mario.dorn@forgefire.net>
2019-07-08 19:49:55 +0200

    Implement DOI resolution to fetch publication metadata

--- apollo-server/context.js
+++ apollo-server/context.js
@@ -1,6 +1,50 @@
+import chalk from 'chalk';
+import { camelCase } from 'change-case';
 import { driver, neo4j } from './utils/neo4j';
 import session from './utils/session';
 import smtp from './utils/smtp';
+import doi from './utils/doi';
+
+async function fetchPublications () {
+  /** @type Session */ let db = driver.session();
+  let queryResult = await db.run(`
+    MATCH (n0:Publication)
+    WHERE n0.doi IS NOT NULL AND (n0.resolved IS NULL OR n0.resolved < (datetime() - duration('P28D')))
+    RETURN collect(n0.doi) AS publications
+  `);
+  let [record] = queryResult.records;
+  let publications = await Promise.all(
+    record.get('publications').map(ref => doi.resolve(ref)),
+  );
+  return publications;
+}
+
+function updatePublications (publications) {
+  let total = 0, successful = 0;
+  publications = publications.map(pub => Object.fromEntries(
+    Object.entries(pub).map(([key, value]) => [camelCase(key), value]),
+  ));
+  // eslint-disable-next-line no-cond-assign
+  if (total = publications.length) {
+    console.log(`Finished querying metadata for a total of ${chalk.yellow(publications.length)} DOIs \\o/`);
+  } else {
+    return total;
+  }
+  publications = publications.filter(pub => !pub.hasError);
+  // eslint-disable-next-line no-cond-assign
+  if (successful = publications.length) {
+    if (successful === total) {
+      console.log(chalk.green('All of the performed queries have been successful!'));
+    } else {
+      console.log(`It seems that ${chalk.green(successful)} of the queries have been successful.`);
+    }
+  } else {
+    console.log(`However ${chalk.red('0')} of the performed queries were successful :-/`);
+  }
+  // TODO: Update database contents with the retrieved citations
+}
+
+fetchPublications().then(updatePublications);
 
 // Context passed to all resolvers (third argument)
 // req => Query
---
+++ apollo-server/utils/doi.js
@@ -0,0 +1,81 @@
+const axios = require('axios');
+const chalk = require('chalk');
+
+const contentTypes = {
+  'csl+json': {
+    header: 'application/vnd.citationstyles.csl+json',
+    process: (DOI, data) => data,
+  },
+  'csl+harvard3': {
+    header: 'text/x-bibliography; style=harvard3',
+    process: (DOI, data) => {
+      return {
+        'DOI': DOI,
+        'formatted-citation': data,
+        'formatted-style': 'harvard3',
+      };
+    },
+  },
+};
+
+const retryStatusCodes = [
+  408, // Request timed out
+  429, // Too Many Requests
+  500, // Internal Server Error
+  503, // Service Unavailable
+  504, // Gateway Timeout
+];
+
+const knownStatusCodes = [
+  ...retryStatusCodes,
+  401, // Unauthorized
+  403, // Forbidden
+  404, // Not Found
+  406, // Unacceptable Type
+  501, // Not Implemented
+];
+
+module.exports = {
+  async resolve(DOI, type = 'csl+harvard3', { resolve, reject } = {}, retry = 0, maxRetries = 5) {
+    let response, result;
+    try {
+      response = await axios.get(`https://doi.org/${DOI}`, {
+        headers: { 'Accept': contentTypes[type].header },
+      });
+      result = contentTypes[type].process(DOI, response.data);
+      let append = retry ? ` after retry ${chalk.yellow('#' + retry)} :)` : '.';
+      console.log(`Received HTTP Status ${chalk.green(response.status)} for `
+        + `DOI ${chalk.blueBright(DOI)} ~ Result has been parsed` + append);
+    } catch (error) {
+      if (error.response && retryStatusCodes.includes(error.response.status) && retry < maxRetries) {
+        let waitSeconds = (++retry * 5) + Math.floor(Math.random() * 25);
+        console.log(`Received HTTP Status ${chalk.red(error.response.status)} for DOI ${chalk.blueBright(DOI)} ~ `
+          + `Retry ${chalk.yellow('#' + retry)} in ${chalk.whiteBright(waitSeconds)} seconds...`);
+        try {
+          result = await new Promise((resolve, reject) => {
+            setTimeout(() => this.resolve(DOI, type, { resolve, reject }, retry, maxRetries), waitSeconds * 1000);
+          });
+        } catch (innerError) {
+          console.error(chalk.red(`Skipped DOI ${chalk.blueBright(DOI)} because of the exception:`), innerError);
+          result = { DOI, hasError: true, error: innerError };
+          if (reject) reject(result);
+          return result;
+        }
+      } else {
+        let messages;
+        if (error.response && knownStatusCodes.includes(error.response.status)) {
+          messages = [`Received HTTP Status ${chalk.red(error.response.status)} for `
+            + `DOI ${chalk.blueBright(DOI)} ~ ${chalk.red('Skipped.')}`];
+        } else {
+          messages = [chalk.red(`Skipped DOI ${chalk.blueBright(DOI)} because of the exception:`), error];
+        }
+        console.error(...messages);
+        result = { DOI, hasError: true, error };
+        if (reject) reject(result);
+        return result;
+      }
+    }
+    if (resolve) resolve(result);
+    return result;
+  },
+};
